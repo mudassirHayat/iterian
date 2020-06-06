6c0885255ed34309996cb29505cb219e398b6d87
mudassirHayat <64309904+mudassirHayat@users.noreply.github.com>
2020-06-06 12:00:50 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     this is mudassar hayat 
+     hello world
+    </div>
+  );
+}
+
+export default App;
